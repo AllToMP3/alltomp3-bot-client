8985fe3ab8537d3eb3474dbaeda747065c4b7a27
Basile Bruneau <basilebruneau@gmail.com>
2020-10-12 23:37:51 +0200

    feat: add auto-reconnect and catch errors

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
-const WebSocketClient = require("websocket").client;
-const storage = require("node-persist");
-const alltomp3 = require("alltomp3");
-const fs = require("fs");
+const WebSocketClient = require('websocket').client;
+const storage = require('node-persist');
+const alltomp3 = require('alltomp3');
+const fs = require('fs');
 
-const SERVER_URL = "wss://server.alltomp3.org/telegram/";
+const SERVER_URL = 'wss://server.alltomp3.org/telegram/';
 const MAX_PARALLEL = 4;
-const TEMP_FOLDER = "/tmp/";
+const TEMP_FOLDER = '/tmp/';
 
 let client = new WebSocketClient();
 let con;
@@ -21,7 +21,7 @@ let processing = {
     this.processNext();
   },
   processQuery: function (q) {
-    console.log("Processing", q.id, q.trackURL || q.singleURL);
+    console.log('Processing', q.id, q.trackURL || q.singleURL);
     this.active += 1;
     if (q.trackURL) {
       var e = alltomp3.downloadTrackURL(q.trackURL, TEMP_FOLDER);
@@ -31,29 +31,29 @@ let processing = {
       return;
     }
     let progress = 0;
-    e.on("download", (infos) => {
+    e.on('download', (infos) => {
       if (infos.progress / 2 - progress > 25) {
         progress = infos.progress / 2;
         senda({ id: q.id, progress: progress });
       }
     });
-    e.on("download-end", (infos) => {
+    e.on('download-end', (infos) => {
       progress = 50;
       senda({ id: q.id, progress: progress });
     });
-    e.on("convert", (infos) => {
+    e.on('convert', (infos) => {
       if (infos.progress / 2 + 50 - progress > 25) {
         progress = infos.progress / 2 + 50;
         senda({ id: q.id, progress: progress });
       }
     });
-    e.once("end", (infos) => {
+    e.once('end', (infos) => {
       const sendFile = (filepath) => {
         fs.readFile(filepath, (err, data) => {
           const bufId = Buffer.from(q.id);
           const len = bufId.length + data.length;
           const buf = Buffer.concat([bufId, data], len);
-          console.log("End of", q.id, "sending buffer of length", len);
+          console.log('End of', q.id, 'sending buffer of length', len);
           con.sendBytes(buf);
           this.active -= 1;
           this.processNext();
@@ -62,21 +62,21 @@ let processing = {
 
       const fileStats = fs.statSync(infos.file);
       if (fileStats.size >= 49 * 1000 * 1000 && q.singleURL) {
-        console.log("File too large: converting to 128 kb/s");
+        console.log('File too large: converting to 128 kb/s');
         // Telegram has a max size of 50 MB
-        const newEvents = alltomp3.downloadAndTagSingleURL(
-          q.singleURL,
-          TEMP_FOLDER,
-          null,
-          null,
-          null,
-          null,
-          { bitrate: "128k" }
-        );
-        newEvents.once("end", (newInfos) => sendFile(newInfos.file));
+        const newEvents = alltomp3.downloadAndTagSingleURL(q.singleURL, TEMP_FOLDER, null, null, null, null, {
+          bitrate: '128k',
+        });
+        newEvents.once('end', (newInfos) => sendFile(newInfos.file));
       } else {
         sendFile(infos.file);
       }
+    });
+    e.on('error', (error) => {
+      senda({ id: q.id, error: error });
+      console.error('An error occured', error);
+      this.active -= 1;
+      this.processNext();
     });
   },
   processNext: function () {
@@ -87,36 +87,45 @@ let processing = {
   },
 };
 
-client.on("connectFailed", function (error) {
-  console.log("Connect Error: " + error.toString());
+client.on('connectFailed', function (error) {
+  console.log('Connect Error: ' + error.toString());
 });
 
-client.on("connect", function (connection) {
+let reconnectTimer;
+
+client.on('connect', function (connection) {
   con = connection;
-  console.log("Connected!\n");
-  connection.on("error", function (error) {
-    console.log("Connection Error: " + error.toString());
+  if (reconnectTimer) {
+    clearInterval(reconnectTimer);
+    reconnectTimer = null;
+  }
+  console.log('Connected!\n');
+  connection.on('error', function (error) {
+    console.log('Connection Error: ' + error.toString());
   });
-  connection.on("close", function () {
-    console.log("echo-protocol Connection Closed");
+  connection.on('close', function () {
+    console.log('Connection closed, retrying in 30s...');
+    reconnectTimer = setInterval(() => {
+      client.connect(SERVER_URL, 'echo-protocol');
+    }, 30000);
   });
-  connection.on("message", function (message) {
-    if (message.type === "utf8") {
+  connection.on('message', function (message) {
+    if (message.type === 'utf8') {
       try {
         var message = JSON.parse(message.utf8Data);
       } catch (e) {}
       if (message.key && message.password && message.confirm) {
-        storage.setItem("credentials", {
+        storage.setItem('credentials', {
           key: message.key,
           password: message.password,
         });
-        storage.setItem("confirm", message.confirm);
+        storage.setItem('confirm', message.confirm);
         console.log(
-          "Now registered.\nTo manage your bot from Telegram, talk to @AllToMP3_bot.\n\nYour key is: " +
+          'Now registered.\nTo manage your bot from Telegram, talk to @AllToMP3_bot.\n\nYour key is: ' +
             message.key +
-            "-" +
+            '-' +
             message.confirm +
-            "\n\n"
+            '\n\n',
         );
       } else if (message.query) {
         processing.newQuery(message.query);
@@ -124,17 +133,15 @@ client.on("connect", function (connection) {
     }
   });
 
-  storage.getItem("credentials", function (err, cred) {
+  storage.getItem('credentials', function (err, cred) {
     if (!cred) {
-      senda({ hello: "world!" });
+      senda({ hello: 'world!' });
     } else {
       credentials = cred;
-      storage.getItem("confirm", function (err, confirm) {
+      storage.getItem('confirm', function (err, confirm) {
         if (confirm) {
           confirmk = confirm;
-          console.log(
-            "Your key is: " + credentials.key + "-" + confirm + "\n\n"
-          );
+          console.log('Your key is: ' + credentials.key + '-' + confirm + '\n\n');
         }
       });
       senda(credentials);
@@ -144,17 +151,17 @@ client.on("connect", function (connection) {
 
 storage
   .init({
-    dir: "./data/",
+    dir: './data/',
     stringify: JSON.stringify,
     parse: JSON.parse,
-    encoding: "utf8",
+    encoding: 'utf8',
   })
   .then(function () {
-    storage.getItem("credentials", function (err, cred) {
+    storage.getItem('credentials', function (err, cred) {
       credentials = cred;
-      storage.getItem("confirm", function (err, confirm) {
+      storage.getItem('confirm', function (err, confirm) {
         confirmk = confirm;
-        client.connect(SERVER_URL, "echo-protocol");
+        client.connect(SERVER_URL, 'echo-protocol');
       });
     });
   });
